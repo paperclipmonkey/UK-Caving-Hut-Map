ad9671332d0507912510a1e71ac3dcb37000d5f3
Michael Waterworth <mike@3equals.co.uk>
2015-12-25 20:31:12 +0000

    Remove Angular

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,25 +1,22 @@
 define([
+  'bootstrap',
   'jquery',
   'jquery-ui',
   'leaflet',
+  'Leaflet.awesome-markers',
+  'leaflet-google',
   'leaflet-sidebar',
-  'json!../geojson/huts.geojson',
-  'angular',
-  'ui-leaflet'
+  'json!../geojson/huts.geojson'
 ], function (
+  bootstrap,
   jquery,
   jqueryui,
   leaflet,
+  awesomeMarkers,
+  leafletgoogle,
   sidebar,
-  geojson,
-  angular,
-  uileaflet
+  geojson
 ) {
-  var app = angular.module('demoapp', ['ui-leaflet'])
-
-  console.log(geojson)
-
-  app.controller('MarkersAddRemoveController', [ '$scope', function ($scope) {
     var tilesDict = {
       satellite: {
         name: 'Satellite',
@@ -60,82 +57,4 @@ define([
         }
       }
     }
-
-    angular.extend($scope, {
-      uk: {
-        lat: 54.5906,
-        lng: -3.0,
-        zoom: 6
-      },
-      layers: {
-        baselayers: {One: tilesDict.satellite, Two: tilesDict.roadmap},
-        overlays: {Four: tilesDict.geology}
-      }
-    })
-
-    var iconsDict = []
-
-    function makeMarker (color) {
-      iconsDict[color] = {
-        iconUrl: 'assets/img/marker-' + color + '.png',
-        iconRetinaUrl: 'assets/img/marker-' + color + '-2x.png',
-        shadowUrl: 'assets/img/marker-shadow.png',
-        iconSize: [25, 41],
-        iconAnchor: [12, 41],
-        popupAnchor: [0, 0]
-      }
-    }
-
-    makeMarker('brown')
-    makeMarker('green')
-
-    angular.extend($scope, {
-      geojson: {
-        data: geojson,
-        pointToLayer: function (feature, latlng) {
-          if (feature.properties.caveclub === 'TRUE') {
-            return new L.Marker(latlng, {icon: L.icon(iconsDict['brown'])})
-          } else {
-            return new L.Marker(latlng, {icon: L.icon(iconsDict['green'])})
-          }
-        },
-        onEachFeature: function (feature, layer) {
-          layer.bindPopup('<h4>' + feature.properties.club + ' - ' + feature.properties.hutname + '</h4>' +
-            '<p>pppn: £' + feature.properties.pppn + '</p>' +
-            '<p>No. bunks: ' + feature.properties.guestbunks + '</p>' +
-            '<p>Drying room: ' + (feature.properties.dryingroom === 'TRUE' ? 'yes' : 'no') + '</p>' +
-            '<p>Address: ' + feature.properties.location + '</p>' +
-            '<p>Grid reference: ' + feature.properties.gridref + '</p>' +
-            "<a href='http://" + feature.properties.website + "'>" + feature.properties.website + '</a>' +
-            "<p><a href='https://github.com/paperclipmonkey/UK-Caving-Hut-Map/blob/master/data/huts.csv'>Update this hut</a></p>"
-          )
-        }
-      }
-    })
-
-    angular.extend($scope, {
-      events: {
-        markers: {
-          enable: ['click'],
-          logic: 'emit'
-        }
-    }})
-
-    $scope.$on('leafletDirectiveGeoJson.click', function (ev, leafletPayload) {
-      console.log('GeoJson Click')
-    })
-
-    $scope.$on('leafletDirectivePath.myMap.mouseover', function (event, path) {
-      console.log('Mouse over')
-    })
-
-    $scope.$on('leafletDirectiveMarker.click', function (event, args) {
-      console.log('Me', $scope.markers[args.markerName])
-    })
-
-    $scope.$on('leafletDirectivePath.click', function (event, link) {
-      console.log('Click')
-    })
-  }
-  ])
 })
--- assets/js/require-config.js
+++ assets/js/require-config.js
@@ -13,6 +13,15 @@ require.config({
       'angular',
       'angular-simple-logger'
     ],
+    'Leaflet.awesome-markers': [
+      'leaflet'
+    ],
+    'leaflet-google': [
+      'leaflet'
+    ],
+    'bootstrap': [
+      'jquery'
+    ],
     'leaflet-sidebar': [
       'leaflet'
     ],
@@ -24,15 +33,11 @@ require.config({
     }
   },
   paths: {
-    angular: '../../bower_components/angular/angular',
-    'angular-animate': '../../bower_components/angular-animate/angular-animate',
-    'angular-resource': '../../bower_components/angular-resource/angular-resource',
-    'angular-route': '../../bower_components/angular-route/angular-route',
+    'leaflet-google': './leaflet-google',
     bootstrap: '../../bower_components/bootstrap/dist/js/bootstrap',
     'bower-leaflet': '../../bower_components/bower-leaflet/leaflet-src',
     jquery: '../../bower_components/jquery/dist/jquery',
     requirejs: '../../bower_components/requirejs/require',
-    'requirejs-text': '../../bower_components/requirejs-text/text',
     json: '../../bower_components/requirejs-plugins/src/json',
     goog: '../../bower_components/requirejs-plugins/src/goog',
     async: '../../bower_components/requirejs-plugins/src/async',
@@ -45,10 +50,17 @@ require.config({
     'Markdown.Converter': '../../bower_components/requirejs-plugins/lib/Markdown.Converter',
     propertyParser: '../../bower_components/requirejs-plugins/src/propertyParser',
     'jquery-ui': '../../bower_components/jquery-ui/jquery-ui',
-    'ui-leaflet': '../../bower_components/ui-leaflet/dist/ui-leaflet',
     leaflet: '../../bower_components/leaflet/dist/leaflet-src',
+    'leaflet-sidebar': '../../bower_components/leaflet-sidebar/src/L.Control.Sidebar',
+    'Leaflet.awesome-markers': '../../bower_components/Leaflet.awesome-markers/dist/leaflet.awesome-markers',
+    'angular-animate': '../../bower_components/angular-animate/angular-animate',
+    'ui-leaflet': '../../bower_components/ui-leaflet/dist/ui-leaflet',
+    'angular-route': '../../bower_components/angular-route/angular-route',
+    angular: '../../bower_components/angular/angular',
     'angular-simple-logger': '../../bower_components/angular-simple-logger/dist/angular-simple-logger',
-    'leaflet-sidebar': '../../bower_components/leaflet-sidebar/src/L.Control.Sidebar'
+    'angular-resource': '../../bower_components/angular-resource/angular-resource'
   },
-  packages: []
+  packages: [
+
+  ]
 })
